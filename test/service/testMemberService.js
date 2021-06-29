4e54f149cfaa45066be6adf054a801a403f9b090
BioCSK <katd6@naver.com>
2021-06-29 14:21:12 +0900

    update

--- test/service/testMemberService.js
+++ test/service/testMemberService.js
@@ -1,16 +1,17 @@
 const assert = require("assert");
 const memberService = require("../../service/memberService");
 const loginFormDto = require("../../model/loginFormDto");
+const bcrypt = require("bcrypt");
 
 describe('test_encryptPassword',function(){
-    it('Should return encryted password',function(done){
+    it('Should return encryted password without changing email',function(done){
         const testPw = "testPw";
         const testEmail = "test@email.com"
          let loginForm = new loginFormDto({email: testEmail ,password : testPw });
          memberService.encryptPassword(loginForm)
             .then((encryptedLoginForm) =>{
                 assert.strictEqual(encryptedLoginForm.getEmail(),testEmail);
-                assert.notStrictEqual(encryptedLoginForm.getPassword(),testPw);
+                assert.strictEqual(true,bcrypt.compareSync(testPw,encryptedLoginForm.getPassword()));
                 done();   
             })
             .catch((err)=>done(err))
